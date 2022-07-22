0b089ff51b65b9cbc1062833ad0c1bd488f6bc04
kaylab78 <kaylabackus78@gmail.com>
2022-07-22 16:16:52 -0500

    Used bcrypt to has passwords for users.

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 // Create User model
@@ -34,6 +35,16 @@ User.init(
         }
     },
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
