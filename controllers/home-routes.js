1887506570a44bf21811e396647074ba347212da
kaylab78 <kaylabackus78@gmail.com>
2022-07-23 15:12:14 -0500

    Used boot camp module 14 lesson 1 to help write the home-routes.js file and start the homepage.handlebars file. Will remove hardcoded data from the home-routes.js file.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,16 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     // Specify which template to use.
-    res.render('homepage');
+    res.render('homepage', {
+        id: 1,
+        post_body: "This is the first time I've used Handlebars.js.",
+        title: "Handlebar.js",
+        created_at: new Date(),
+        comments: [{}, {}],
+        user: {
+            username: 'newwebdev'
+        }
+    });
 });
 
 module.exports = router;
