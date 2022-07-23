67b5006de1e9999af04352f8187a97a69842d836
kaylab78 <kaylabackus78@gmail.com>
2022-07-23 09:45:30 -0500

    Created and tested associations between User and Post models.

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// A user can have many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// A post can only have one user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
+// { force: true } makes the tables recreate if there are association changes. See boot camp module 13.3.5
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
