ba40eb59c8accae1957c6f2f36a278e5e9053478
kaylab78 <kaylabackus78@gmail.com>
2022-07-23 12:46:43 -0500

    Created Comment model and associations. Tested with 'node server,' and it works.

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 // A user can have many posts
 User.hasMany(Post, {
@@ -11,4 +12,24 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+// A comment can only have one user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// A comment can only belong to one post
+Comment.belongsTo(User, {
+    foreignKey: 'post_id'
+});
+
+// A user can have many comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+// A post can have many comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
