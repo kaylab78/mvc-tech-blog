76251f54477bddcd82692db36a322cd3c731eb5b
kaylab78 <kaylabackus78@gmail.com>
2022-07-23 10:31:06 -0500

    Created and tested POST route for new posts.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -19,4 +19,58 @@ router.get('/', (req, res) => {
         });
 });
 
+// GET /api/posts/1
+router.get('/:id', (req, res) => {
+    const postId = req.params.id;
+
+    Post.findOne({
+        where: {
+            id: postId
+        },
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: "We can't find a post with this id." });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// POST /api/posts
+router.post('/', (req, res) => {
+    const title = req.body.title;
+    const postUrl = req.body.post_url;
+    const userId = req.body.user_id;
+
+    /* JSON body
+    {
+        "title" : "Article Title",
+        "post_url" : "https://google.com",
+        "user_id" : 1
+    }
+    */
+   Post.create({
+    title: title,
+    post_url: postUrl,
+    user_id: userId
+   })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
