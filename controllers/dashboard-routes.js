525b10dd10940e7570c910b51932badd31c2042d
kaylab78 <kaylabackus78@gmail.com>
2022-07-24 11:51:32 -0500

    Updated dashboard route to show user posts on their dashboard.

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,10 +2,40 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
-// GET /
+// GET /dashboard
 router.get('/', (req, res) => {
-    // loggedIn is hardcoded as true because the user can't get to the dashboard unless they're logged in per boot camp module 14.5.3
-    res.render('dashboard', { loggedIn: true });
+    const userId = req.session.user_id;
+
+    Post.findAll({
+        where: {
+            user_id: userId
+        },
+        attributes: ['id', 'title', 'post_body', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            // Serialize data before passing to template
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            // loggedIn is hardcoded as true because the user can't get to the dashboard unless they're logged in per boot camp module 14.5.3
+            res.render('dashboard', { posts, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
