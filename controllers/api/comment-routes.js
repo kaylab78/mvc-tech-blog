d9fb1d2d28bc124d72284c45185b98f97d1c1e7c
kaylab78 <kaylabackus78@gmail.com>
2022-07-24 09:48:45 -0500

    Attempted to add functionality to comment form, but it's not working. Will need to ask tutor or TA's for help.

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -13,27 +13,30 @@ router.get('/', (req, res) => {
 
 // POST /api/comments
 router.post('/', (req, res) => {
-    const commentText = req.body.comment_text;
-    const userId = req.body.user_id;
-    const postId = req.body.post_id;
+    // Check the session
+    if (req.session) {
+        const commentText = req.body.comment_text;
+        const userId = req.session.user_id;
+        const postId = req.body.post_id;
 
-    /* JSON body
-    {
-        "comment_text" : "This is a comment.",
-        "user_id" : 1,
-        "post_id" : 1
+        /* JSON body
+        {
+            "comment_text" : "This is a comment.",
+            "user_id" : 1,
+            "post_id" : 1
+        }
+        */
+        Comment.create({
+            comment_text: commentText,
+            user_id: userId,
+            post_id: postId
+        })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
     }
-    */
-   Comment.create({
-    comment_text: commentText,
-    user_id: userId,
-    post_id: postId
-   })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
 });
 
 // DELETE /api/comments/1
--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -7,7 +7,25 @@ async function commentFormHandler(event) {
         window.location.toString().split('/').length - 1
     ];
 
-    console.log(comment_text, post_id);
-}
+    // console.log(comment_text, post_id);
+    if (comment_text) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                post_id,
+                comment_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
 
 document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
