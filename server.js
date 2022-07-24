f72d137b8aeba78a0230f5f481c910f4b8de66f1
kaylab78 <kaylabackus78@gmail.com>
2022-07-24 11:12:17 -0500

    Added helpers to server.js and implemented in comments.handlebars and post-info.handlebars. Tested and the dates are now MM/DD/YYYY.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
+const sequelize = require('./config/connection');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
