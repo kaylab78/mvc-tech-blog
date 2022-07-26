0ba18dc48c5dbfb3e3bfa70df6031af383726acf
kaylab78 <kaylabackus78@gmail.com>
2022-07-25 19:20:37 -0500

    Fixed Save button on edit post page. The JSON.stringify needed to match the column named declared in the Post model. Thank you to boot camp tutor Megan for her help and patience

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,16 +2,19 @@ async function editFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value.trim();
-    const postBody = document.querySelector('input[name="post-body"]').value.trim();
+    // const postBody = document.querySelector('#postBody').value.trim();
+    const post_body = document.querySelector('input[name="post-body"]').value.trim();
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length -1
     ];
 
-    const response = await fetch(`api/posts/${id}`, {
+    console.log(post_body);
+
+    const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
             title,
-            postBody
+            post_body
         }),
         headers: {
             'Content-Type': 'application/json'
